c8a87a7421bd31606affca6a9153d18814071ab3
Peter <pedamain@gmx.at>
2020-11-23 18:41:58 +0100

    first commit

---
+++ src/main/java/Order.java
@@ -0,0 +1,13 @@
+public class Order {
+
+
+    String orderName;
+
+    public Order(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+}
